b2718720df2ee60bdceb8ffb63e6367e51b84ff6
bhaskarjb <hcbhaskar7@gmail.com>
2018-10-14 23:57:30 +0530

    yaml_files_mod

--- routes/yaml.js
+++ routes/yaml.js
@@ -6,8 +6,8 @@ router.get('/yaml', (req, res) => {
     res.status(200).json({ 
         status: 200, 
         workloadName: "cockroachdb",
-        nameSpaceyaml: "https://github.com/openebs/e2e-infrastructure/blob/bb9f737000fa32fd71fac104b6ec0567997960da/production/percona-jiva/percona-jiva-namespace.yaml",
-        workloadyaml:"https://github.com/openebs/e2e-infrastructure/blob/7c67f70c877493f39c634ffce6305ee34e74d9ea/production/percona-jiva/percona-openebs-deployment.yaml"
+        nameSpaceyaml: "https://raw.githubusercontent.com/bhaskarhc/yaml-cockroach/master/cock-namespace.yaml",
+        workloadyaml:"https://raw.githubusercontent.com/bhaskarhc/yaml-cockroach/master/cockroachdb-sts.yaml"
  });
 });
 
